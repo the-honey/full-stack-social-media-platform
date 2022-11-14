c3480a98a8ee0d68d60a8d869c8c7c32162938a3
István Mézes <1valaki1asd@gmail.com>
2022-11-14 23:53:29 +0100

    Add basic auth

--- src/api/src/controllers/auth.ts
+++ src/api/src/controllers/auth.ts
@@ -1,22 +1,94 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, response, Response } from "express";
 import { prisma } from "../db";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  let count: number = await prisma.user.count({
-    where: {
-      OR: [{ username: req.body.username }, { email: req.body.email }],
-    },
-  });
+  prisma.user
+    .count({
+      where: {
+        OR: [
+          { username: req.body.username ?? "" },
+          { email: req.body.email ?? "" },
+        ],
+      },
+    })
+    .then((count) => {
+      if (count > 0) {
+        res.status(409).send({ message: "User already exists." });
+      }
+
+      // TODO: input validation
+
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+      prisma.user
+        .create({
+          data: {
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword,
+            profile: {
+              create: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                brithDate: new Date(req.body.birthDate),
+              },
+            },
+          },
+        })
+        .then(() =>
+          res.status(200).send({ message: "User successfully created." })
+        );
+    });
 };
 
 export const login = (req: Request, res: Response) => {
-  // TODO
+  prisma.user
+    .findFirst({
+      where: {
+        OR: [{ username: req.body.username }, { email: req.body.email }],
+      },
+    })
+    .then((user) => {
+      if (!user) res.status(404).send({ message: "User was not found." });
+
+      const checkPassword = bcrypt.compareSync(
+        req.body.password,
+        user?.password ?? ""
+      );
+
+      if (!checkPassword)
+        return res.status(400).send({ message: "Password is incorrect." });
+
+      const token = jwt.sign(
+        { id: user?.id ?? "" },
+        process.env.SECRET_KEY ?? ""
+      );
+
+      // @ts-ignore TEMPORARY FIX
+      const { password, ...others } = user;
+
+      res
+        .cookie("accessToken", token, {
+          httpOnly: true,
+        })
+        .status(200)
+        .send(others);
+    });
 };
 
 export const logout = (req: Request, res: Response) => {
-  // TODO
+  res
+    .clearCookie("accessToken", {
+      secure: true,
+      sameSite: "none",
+    })
+    .status(200)
+    .send({ message: "User has been logged out." });
 };
--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -1,10 +1,16 @@
 import express, { Application, NextFunction, Request, Response } from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
 
 const app: Application = express();
 
 import authRoutes from "./routes/auth";
 
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send({ message: "Internal server error." });
@@ -12,6 +18,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 
 app.use("/api/auth", authRoutes);
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("The API has started.");
 });
