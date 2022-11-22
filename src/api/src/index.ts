158dff13e213df1b85709653f7fadc55a194ca7b
TheHoney <1valaki1asd@gmail.com>
2022-11-23 00:11:16 +0100

    Implement basic file upload

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -2,6 +2,8 @@ import express, { Application, NextFunction, Request, Response } from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import multer from "multer";
+import path from "path";
 dotenv.config();
 
 const app: Application = express();
@@ -14,9 +16,35 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage, limits: { fileSize: 10_000_000 } });
+
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send({ message: "Internal server error." });
 });
+
+app.post(
+  "/api/upload",
+  upload.single("file"),
+  (req: Request, res: Response, next: NextFunction) => {
+    const file = req.file;
+    if (!file)
+      return res.status(400).json({ message: "Something went wrong." });
+    res.status(200).json({ message: "File has been uploaded.", path: file });
+  }
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
