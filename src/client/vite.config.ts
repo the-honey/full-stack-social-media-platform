a741584e2f505ad4234466fdbd3fcb3919256b56
István Mézes <1valaki1asd@gmail.com>
2022-11-29 17:24:36 +0100

    Basic configuration

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@/assets': path.resolve(__dirname, './assets/'),
+      '@/pages': path.resolve(__dirname, './src/pages/'),
+      '@/components': path.resolve(__dirname, './src/components/'),
+    },
+  },
+});
