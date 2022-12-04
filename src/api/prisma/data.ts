44935bea42175aef143f65aced4c48c4f9d15b52
István Mézes <1valaki1asd@gmail.com>
2022-12-05 00:10:56 +0100

    Add prisma seed

---
+++ src/api/prisma/data.ts
@@ -0,0 +1,148 @@
+export const data = [
+  {
+    user: {
+      username: 'thehoney',
+      email: 'istvan.mezes0612@gmail.com',
+      password: '$2a$10$FSTy7IsDPz1AZzYP4sk9v.EATnCNv4e9dq9BOsj59WoexzwPeJFZC',
+      isEmailVerified: true,
+      createdAt: new Date('2022-12-01'),
+    },
+    profile: {
+      firstName: 'István',
+      lastName: 'Mézes',
+      description: '🎓ELTE IK',
+      profilePicUrl: null,
+      birthDate: new Date('2000-06-12'),
+    },
+    posts: [
+      {
+        content: 'This is my first post.',
+        createdAt: new Date('2022-12-01 12:00:00'),
+      },
+    ],
+  },
+  {
+    user: {
+      username: 'testuser',
+      email: 'test@test.com',
+      password: '$2a$10$FSTy7IsDPz1AZzYP4sk9v.EATnCNv4e9dq9BOsj59WoexzwPeJFZC',
+      isEmailVerified: true,
+      createdAt: new Date('2022-12-02'),
+    },
+    profile: {
+      firstName: 'Test',
+      lastName: 'Test',
+      description: null,
+      profilePicUrl: null,
+      birthDate: new Date('1970-01-01'),
+    },
+    posts: [
+      {
+        content: 'Hello!',
+        createdAt: new Date('2022-12-02 13:00:00'),
+        updatedAt: new Date('2022-12-02 13:00:00'),
+      },
+      {
+        content: 'This is a test.',
+        createdAt: new Date('2022-12-02 13:30:00'),
+        updatedAt: new Date('2022-12-02 13:30:00'),
+      },
+    ],
+  },
+  {
+    user: {
+      username: 'testuser2',
+      email: 'test2@test.com',
+      password: '$2a$10$FSTy7IsDPz1AZzYP4sk9v.EATnCNv4e9dq9BOsj59WoexzwPeJFZC',
+      isEmailVerified: true,
+      createdAt: new Date('2022-12-03'),
+    },
+    profile: {
+      firstName: 'Test',
+      lastName: 'Test',
+      description: null,
+      profilePicUrl: null,
+      birthDate: new Date('1970-01-01'),
+    },
+    posts: [
+      {
+        content: 'Hello!',
+        updatedAt: new Date('2022-12-03 14:30:00'),
+        createdAt: new Date('2022-12-03 14:30:00'),
+      },
+      {
+        content: 'This is a test.',
+        updatedAt: new Date('2022-12-03 14:50:00'),
+        createdAt: new Date('2022-12-03 14:50:00'),
+      },
+    ],
+  },
+  {
+    user: {
+      username: 'testuser3',
+      email: 'test3@test.com',
+      password: '$2a$10$FSTy7IsDPz1AZzYP4sk9v.EATnCNv4e9dq9BOsj59WoexzwPeJFZC',
+      isEmailVerified: true,
+      createdAt: new Date('2022-12-04'),
+    },
+    profile: {
+      firstName: 'Spam',
+      lastName: 'Bot',
+      description: 'Spamming bot',
+      profilePicUrl: null,
+      birthDate: new Date('1970-01-01'),
+    },
+    posts: [
+      {
+        content: 'I have a lot of posts!',
+        updatedAt: new Date('2022-12-04 12:00:00'),
+        createdAt: new Date('2022-12-04 12:00:00'),
+      },
+      {
+        content: 'I have a lot of posts!',
+        updatedAt: new Date('2022-12-04 12:01:00'),
+        createdAt: new Date('2022-12-04 12:01:00'),
+      },
+      {
+        content: 'I have a lot of posts!',
+        updatedAt: new Date('2022-12-04 12:02:00'),
+        createdAt: new Date('2022-12-04 12:02:00'),
+      },
+      {
+        content: 'I have a lot of posts!',
+        updatedAt: new Date('2022-12-04 12:03:00'),
+        createdAt: new Date('2022-12-04 12:03:00'),
+      },
+      {
+        content: 'I have a lot of posts!',
+        updatedAt: new Date('2022-12-04 12:04:00'),
+        createdAt: new Date('2022-12-04 12:04:00'),
+      },
+      {
+        content: 'I have a lot of posts!',
+        updatedAt: new Date('2022-12-04 12:05:00'),
+        createdAt: new Date('2022-12-04 12:05:00'),
+      },
+      {
+        content: 'I have a lot of posts!',
+        updatedAt: new Date('2022-12-04 12:06:00'),
+        createdAt: new Date('2022-12-04 12:06:00'),
+      },
+      {
+        content: 'I have a lot of posts!',
+        updatedAt: new Date('2022-12-04 12:07:00'),
+        createdAt: new Date('2022-12-04 12:07:00'),
+      },
+      {
+        content: 'I have a lot of posts!',
+        updatedAt: new Date('2022-12-04 12:08:00'),
+        createdAt: new Date('2022-12-04 12:08:00'),
+      },
+      {
+        content: 'I have a lot of posts!',
+        updatedAt: new Date('2022-12-04 12:09:00'),
+        createdAt: new Date('2022-12-04 12:09:00'),
+      },
+    ],
+  },
+];
