83a0c9065ffc0ccbc2f58e3b9cc1002209753b1a
István Mézes <1valaki1asd@gmail.com>
2022-12-28 21:49:48 +0100

    Add data for tests

--- src/api/tests/data.ts
+++ src/api/tests/data.ts
@@ -1,4 +1,11 @@
-import { User } from '@prisma/client';
+import {
+  Reaction,
+  User,
+  Post,
+  Comment,
+  Profile,
+  Follows,
+} from '@prisma/client';
 
 const user: User = {
   id: '1',
@@ -25,4 +32,51 @@ const loginInput = {
   password: 'password',
 };
 
-export { user, registerInput, loginInput };
+const post: Post = {
+  id: '1',
+  authorId: '1',
+  content: 'this is a post',
+  createdAt: new Date('2022-12-01 00:00:00'),
+  updatedAt: new Date('2022-12-01 00:00:00'),
+};
+
+const reaction: Reaction = {
+  authorId: '1',
+  postId: '1',
+  createdAt: new Date('2022-12-01 00:00:00'),
+  type: 'LIKE',
+};
+
+const comment: Comment = {
+  id: '1',
+  authorId: '1',
+  postId: '1',
+  createdAt: new Date('2022-12-01 00:00:00'),
+  updatedAt: new Date('2022-12-01 00:00:00'),
+  content: 'this is a comment',
+};
+
+const profile: Profile = {
+  id: 1,
+  firstName: 'Test',
+  lastName: 'Test',
+  description: null,
+  birthDate: new Date('2000-06-12'),
+  profilePicUrl: null,
+};
+
+const follow: Follows = {
+  followerId: '1',
+  followingId: '2',
+};
+
+export {
+  user,
+  registerInput,
+  loginInput,
+  post,
+  reaction,
+  comment,
+  profile,
+  follow,
+};
